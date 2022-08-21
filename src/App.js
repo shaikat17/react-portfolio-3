fa108b89b6c13141c8f0bf5d73ae900eab9bf909
shaikat17 <shaikatpal56@gmail.com>
2022-08-21 12:29:05 +0600

    react router and material ui added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 import './App.css';
 import Header from './components/home/header/Header';
 
--- src/components/home/header/Header.jsx
+++ src/components/home/header/Header.jsx
@@ -1,25 +1,30 @@
 import React, { useState } from 'react'
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+} from "react-router-dom";
 
 const Header = () => {
     const [sideBar, setSideBar] = useState('false')
-  return (
-    <>
-        <header className="header">
-            <div className="container flex">
-                <div className="logo">
-                    <img src="assets/logo.png" alt="" srcset="" />
+    return (
+        <>
+            <header className="header">
+                <div className="container flex">
+                    <div className="logo">
+                        <img src="assets/logo.png" alt="" srcset="" />
+                    </div>
+                    <div className="nav">
+                        <ul className={sideBar ? "nav-links-sidebar" : "nav-links"} onClick={() => setSideBar(false)} >
+                            <li>
+                                <a href="/">Home</a>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
-                <div className="nav">
-                    <ul className={sideBar ? "nav-links-sidebar" : "nav-links"} onClick={() => setSideBar(false)} >
-                        <li>
-                            <a href="/">Home</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </header>
-    </>
-  )
+            </header>
+        </>
+    )
 }
 
 export default Header
