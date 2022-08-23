f76dbb01393462cda7595aa9bf34c1dfcc0e074b
shaikat17 <shaikatpal56@gmail.com>
2022-08-23 13:49:18 +0600

    Skill Section Completed

--- src/components/home/Skill.jsx
+++ src/components/home/Skill.jsx
@@ -1,19 +1,22 @@
 import React, { Fragment } from "react";
 
 const Skill = () => {
-
-    const data = [
-        {
-          title: "Every Day is a New Challenge",
-          para: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id autem veritatis nobis inventore consectetur omnis porro iusto excepturi, quo voluptate quos vel, quasi officia fugiat.",
-          para1: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos illo qui quibusdam perspiciatis soluta cumque quia asperiores sapiente! Veniam odit veritatis mollitia eos similique impedit beatae iusto.",
-        },
-      ]
+  const data = [
+    {
+      title: "Every Day is a New Challenge",
+      para: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id autem veritatis nobis inventore consectetur omnis porro iusto excepturi, quo voluptate quos vel, quasi officia fugiat.",
+      para1:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos illo qui quibusdam perspiciatis soluta cumque quia asperiores sapiente! Veniam odit veritatis mollitia eos similique impedit beatae iusto.",
+    },
+  ];
 
   const Progress = ({ done, title }) => {
     return (
       <div className="progress">
-        <div className="progress-done" style={{ opacity: 1, width: `${done}%` }}>
+        <div
+          className="progress-done"
+          style={{ opacity: 1, width: `${done}%` }}
+        >
           <h4>{title}</h4>
           <h4>{done} %</h4>
         </div>
@@ -31,15 +34,20 @@ const Skill = () => {
           </div>
           <div className="content flex">
             <div className="left topMargin">
-              <Progress done='90' title="HTML" />
-              <Progress done='85' title="CSS" />
-              <Progress done='70' title="Javascript" />
-              <Progress done='68' title="React JS" />
+              <Progress done="90" title="HTML" />
+              <Progress done="85" title="CSS" />
+              <Progress done="70" title="Javascript" />
+              <Progress done="68" title="React JS" />
             </div>
             <div className="right mtop">
-            {data.map((val) => {
-                
-            })}
+              {data.map((val) => {
+                return (<Fragment>
+                  <h1>{val.title}</h1>
+                  <p>{val.para}</p>
+                  <p>{val.para1}</p>
+                  <button className="primary-btn">Hire Me</button>
+                </Fragment>)
+              })}
             </div>
           </div>
         </div>
